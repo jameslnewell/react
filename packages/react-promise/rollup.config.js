cf55e85e71c80a7360da13d2963666f95c4e0743
James Newell <james@digitaledgeit.com.au>
2019-06-06 23:13:21 +1000

    package without polyfills

--- packages/react-promise/rollup.config.js
+++ packages/react-promise/rollup.config.js
@@ -1,31 +1,35 @@
 import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
-import {dependencies, peerDependencies} from './package.json';
+import pkg from './package.json';
 
 const extensions = ['.ts', '.tsx', '.mjs', '.js', '.jsx', '.json'];
 
 export default {
   input: `src/index.ts`,
-  output: [
-    {
-      file: `lib/index.js`,
-      format: 'cjs',
-    },
-    {
-      file: `lib/index.mjs`,
-      format: 'esm',
-    },
-  ],
   external: [
-    ...Object.keys(dependencies || {}),
-    ...Object.keys(peerDependencies || {}),
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
   ],
   plugins: [
-    babel({
-      extensions,
-    }),
     resolve({
       extensions,
     }),
+    commonjs({inlude: 'node_modules/**'}),
+    babel({
+      extensions,
+      include: 'src/**/*',
+      exclude: 'node_modules/**',
+    }),
+  ],
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+    },
   ],
 };
