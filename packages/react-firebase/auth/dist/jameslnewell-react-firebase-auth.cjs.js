29725914b7b11489f47b22f6d4e849b228d3bf8b
James Newell <james@jameslnewell.dev>
2021-01-20 23:50:23 +1100

    removed unused files

--- packages/react-firebase/auth/dist/jameslnewell-react-firebase-auth.cjs.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./jameslnewell-react-firebase-auth.cjs.prod.js');
-} else {
-  module.exports = require('./jameslnewell-react-firebase-auth.cjs.dev.js');
-}
