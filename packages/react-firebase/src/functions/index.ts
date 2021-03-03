b0597fc72e54b686bfad271f6bd7baeae3854ee6
James Newell <james@jameslnewell.dev>
2021-03-03 20:24:57 +1100

    added functions

---
+++ packages/react-firebase/src/functions/index.ts
@@ -0,0 +1,12 @@
+// import { useDeferredPromise, usePromise } from '@jameslnewell/react-promise';
+// import firebase from 'firebase/app';
+// import 'firebase/functions';
+
+// export function useFunction(name: string, data: {}, options: {}) {
+//   return usePromise(['@jameslnewell/react-firebase/functions', name, JSON.stringify(data)], () => firebase.functions().httpsCallable(name, options)(data));
+// }
+
+// export function useDeferredFunction(name: string, options: {}) {
+//   return useDeferredPromise(['@jameslnewell/react-firebase/functions', name, JSON.stringify(data)], (data) => firebase.functions().httpsCallable(name, options)(data));
+// }
+export const __unstable__ = {};
