b28bcdefcff7a75dbe1eec35f5a4c34ee35ed2bc
James Newell <james@jameslnewell.dev>
2021-07-15 01:13:13 +1000

    add prettier

---
+++ prettier.config.js
@@ -0,0 +1,2 @@
+/* eslint-env node */
+module.exports = require('@jameslnewell/prettier-config');
--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -1,5 +1,7 @@
 /* eslint-env node */
 module.exports = {
   stories: ['../../packages/*/src/**/*.stories.@(ts|tsx)'],
-  core: {builder: 'webpack5'},
+  core: {
+    builder: 'webpack5',
+  },
 };
